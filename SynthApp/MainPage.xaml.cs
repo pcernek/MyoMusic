4590666ae3496579f918feda5a9294f7b720e64f
mercurial-zephyr <ptcernek@gmail.com>
2015-10-18 05:06:08 -0700

    add keyboard integration

--- SynthApp/MainPage.xaml.cs
+++ SynthApp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.System;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,10 +27,32 @@ namespace SynthApp
         public MainPage()
         {
             this.InitializeComponent();
+            
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Set the input focus to ensure that keyboard events are raised.
+            this.Loaded += delegate { this.Focus(FocusState.Programmatic); };
         }
 
         private void Grid_KeyDown(object sender, KeyRoutedEventArgs e)
         {
+            switch (e.Key)
+            {
+                case VirtualKey.W:
+                    ColorCanvas.Background = new SolidColorBrush(Colors.Green);
+                    break;
+                case VirtualKey.A:
+                    ColorCanvas.Background = new SolidColorBrush(Colors.AliceBlue);
+                    break;
+                case VirtualKey.S:
+                    ColorCanvas.Background = new SolidColorBrush(Colors.Magenta);
+                    break;
+                case VirtualKey.D:
+                    ColorCanvas.Background = new SolidColorBrush(Colors.Yellow);
+                    break;
+            }
 
         }
 
@@ -36,5 +60,6 @@ namespace SynthApp
         {
 
         }
+        
     }
 }
